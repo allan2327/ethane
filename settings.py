0a1f4d03402cf09fa341635bfb663b145720cf11
Vincent Perez <perezv815@gmail.com>
2017-07-18 00:26:51 +0200

    Configure webpack for react frontend

--- settings.py
+++ settings.py
@@ -69,6 +69,9 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'DIRS': (
+            BASE_DIR + '/templates/',
+        ),
     },
 ]
 
@@ -123,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = 'static'
+
+STATICFILES_DIRS = [
+    'tokens/react/static',
+]
